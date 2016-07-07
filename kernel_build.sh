b5df0cc2b1a7add305b96006e5a77ca6b774687a
Rafael Julio <Rafael_Julio11@hotmail.com>
2016-07-06 23:04:52 -0300

    Initial Work
    - moto x 2013 (ghost) default manifest (mm)
    - toolchain manifest
    - kernel build script

---
+++ kernel_build.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+# Original script: Megatron007 @ XDA
+# This is a scrpt used to build custom kernels, if you are gonna use it, you need to change the lines 15, 16 and 23. According with your environment.
+
+echo export KBUILD_BUILD_USER=rafael
+export KBUILD_BUILD_USER=rafael
+
+echo  export KBUILD_BUILD_HOST=Inspiron
+export KBUILD_BUILD_HOST=Inspiron
+
+echo '#############'
+echo 'Setting Up'
+echo '#############'
+export ARCH=arm
+export CROSS_COMPILE=~/Development/arm-eabi-4.9/bin/arm-eabi-
+
+###### defines ######
+
+local_dir=$PWD
+defconfig=msm8960dt_mmi_defconfig
+jobs=4
+AnyKernel_folder=~/Development/AnyKernel
+KERNEL=moto_x_kernel
+
+###### defines ######
+
+echo ''
+echo '#############'
+echo 'Cleaning'
+echo '#############'
+make clean                                                                 # clean the sources
+rm -rf out
+rm $AnyKernel_folder/$KERNEL.zip                                     # clean the output folder
+rm $AnyKernel_folder/modules/*
+rm $AnyKernel_folder/zImage
+echo ''
+echo '#############'
+echo 'Setting up'
+echo '#############'
+make $defconfig
+echo ''
+echo '#############'
+echo 'Compiling'
+echo '#############'
+time make -j$jobs
+echo ''
+echo '#############'
+echo 'Post-compile'
+echo '#############'
+echo ''
+mkdir -p out/modules                                                       # make "out" folder
+cp arch/arm/boot/zImage out/zImage                                         # copy zImage to "out" folder
+# Find and copy modules
+find -name '*.ko' | xargs -I {} cp {} ./out/modules/
+cp -r out/* $AnyKernel_folder/                                       # copy zImage and modules to AnyKernel folder
+echo 'Done'
+echo ''
+if [ -a arch/arm/boot/zImage ]; then
+echo '#############'
+echo 'Making zip'
+echo '#############'
+echo ''
+cd $AnyKernel_folder/
+zip -r $KERNEL.zip ./ -x *.zip *.gitignore *EMPTY_DIRECTORY
+if [[ $1 = -d ]]; then
+cp $KERNEL $AnyKernel_folder/$KERNEL
+echo "Copying $KERNEL to My Folder"
+fi
+cd $local_dir                                                              # cd back to the old dir
+echo ''
+echo '#############'
+echo 'Success!'
+echo '#############'
+else
+echo ''
+echo '#############'
+echo 'Fail!'
+echo '#############'
+fi
