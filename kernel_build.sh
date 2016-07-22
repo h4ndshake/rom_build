525acae91e789d2d87a7b9a074810d750c0475d8
Rafael Julio <Rafael_Julio11@hotmail.com>
2016-07-22 17:19:16 -0300

    Kernel script updates

--- kernel_build.sh
+++ kernel_build.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
 # Original script: Megatron007 @ XDA
-# This is a scrpt used to build custom kernels, if you are gonna use it, you need to change the lines 15, 16 and 23. According with your environment.
+# This is a script used to build custom kernels.
+###### defines ######
+
+local_dir=$PWD
+defconfig=msm8960dt_mmi_defconfig
+jobs=4
+AnyKernel_folder=~/Development/AnyKernel
+KERNEL=XPerience-07x
+
+###### defines ######
 
 echo export KBUILD_BUILD_USER=rafael
 export KBUILD_BUILD_USER=rafael
@@ -10,34 +19,19 @@ echo  export KBUILD_BUILD_HOST=Inspiron
 export KBUILD_BUILD_HOST=Inspiron
 
 echo '#############'
-echo 'Setting Up'
-echo '#############'
-export ARCH=arm
-export CROSS_COMPILE=~/Development/arm-eabi-4.9/bin/arm-eabi-
-
-###### defines ######
-
-local_dir=$PWD
-defconfig=msm8960dt_mmi_defconfig
-jobs=4
-AnyKernel_folder=~/Development/AnyKernel
-KERNEL=moto_x_kernel
-
-###### defines ######
-
-echo ''
-echo '#############'
 echo 'Cleaning'
 echo '#############'
-make clean                                                                 # clean the sources
+make clean                                                           # clean the sources
 rm -rf out
 rm $AnyKernel_folder/$KERNEL.zip                                     # clean the output folder
 rm $AnyKernel_folder/modules/*
 rm $AnyKernel_folder/zImage
 echo ''
 echo '#############'
-echo 'Setting up'
+echo 'Setting Up'
 echo '#############'
+export ARCH=arm
+export CROSS_COMPILE=~/Development/arm-eabi-5.x/bin/arm-eabi-
 make $defconfig
 echo ''
 echo '#############'
@@ -49,8 +43,8 @@ echo '#############'
 echo 'Post-compile'
 echo '#############'
 echo ''
-mkdir -p out/modules                                                       # make "out" folder
-cp arch/arm/boot/zImage out/zImage                                         # copy zImage to "out" folder
+mkdir -p out/modules                                                 # make "out" folder
+cp arch/arm/boot/zImage out/zImage                                   # copy zImage to "out" folder
 # Find and copy modules
 find -name '*.ko' | xargs -I {} cp {} ./out/modules/
 cp -r out/* $AnyKernel_folder/                                       # copy zImage and modules to AnyKernel folder
@@ -67,7 +61,7 @@ if [[ $1 = -d ]]; then
 cp $KERNEL $AnyKernel_folder/$KERNEL
 echo "Copying $KERNEL to My Folder"
 fi
-cd $local_dir                                                              # cd back to the old dir
+cd $local_dir                                                        # cd back to the old dir
 echo ''
 echo '#############'
 echo 'Success!'
